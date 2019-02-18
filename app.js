26f58e2e8fe74137d0ab7731aa33fa29a8acc963
Anurag Gupta <anurag01gupta@github.com>
2019-02-18 20:43:05 +0530

    angularjs version

---
+++ app.js
@@ -0,0 +1,55 @@
+var app = angular.module('anuraggupta', ['ui.router', 'ncy-angular-breadcrumb']);
+app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {
+    'use strict';
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};    
+    }
+    // stop cache data storage for browsers
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    $stateProvider.state('/', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'mainController',
+        ncyBreadcrumb: {
+          label: 'Home'
+        }
+    });
+    $stateProvider.state('archive', {
+        url: '/archive',
+        templateUrl: 'archive.html',
+        controller: 'mainController',
+        ncyBreadcrumb: {
+          label: 'Archive'
+        }
+    });
+    $stateProvider.state('archive.angularjs', {
+        url: '/angularjs',
+        templateUrl: 'angularjs.html',
+        controller: 'mainController',
+        ncyBreadcrumb: {
+          label: 'AngularJs'
+        }
+    });
+    $urlRouterProvider.otherwise('/');
+}]);
+app.controller("mainController", ['$scope', '$state', function($scope, $state){
+    $scope.$state = $state;
+    $scope.userInfo = {
+        firstName: "Anurag",
+        lastName: "Gupta",
+        designation: "Programmer / Web Developer",
+        description:""
+    };
+
+    $scope.articals = [
+        {
+            title: "Angular Js",
+            ui_sref: "archive.angularjs",
+            subtitles: [{
+                title: "Difference between One way and Two way data binding"
+            }]
+        }
+]   ;
+}]);
